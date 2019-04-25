acb0cb8317dd907e24eccfdc5402d9a2c697a405
Rodrigo Taipe <rt363@cornell.edu>
2019-04-25 17:54:02 -0400

    Finish first sprint

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 import json
-from db import db, User, Colleagues
+from db import db, User
 from flask import Flask, request
 
 
@@ -15,82 +15,82 @@ with app.app_context():
     db.create_all()
 
 
-@app.route('/api/users/')
+@app.route('/api/users/', methods=['GET'])
 def get_all_users():
     users = User.query.all()
-    res = {'success': True, 'data': [user.serialize() for user in users]}
+    res = {'success': True, 'data': [user.serialize(True) for user in users]}
     return json.dumps(res), 200
-    
-@app.route('/api/users/', methods = ['POST'])
+
+
+@app.route('/api/users/', methods=['POST'])
 def create_user():
     user_dict = json.loads(request.data)
-    
+    print(user_dict)
     user_info = User(
-        name = user_dict.get('name',"anon"),
-        email = user_dict.get('email',""),
-        phone = user_dict.get('phone',""),
-        company = user_dict.get('company',""),
+        name=user_dict.get('name', "Robin Zheng"),
+        email=user_dict.get('email', "rjz47@cornell.edu"),
+        phone=user_dict.get('phone', "860-278-6106"),
+        company=user_dict.get('company', "Cornell University"),
+        imgURL=user_dict.get('imgURL', "feelsbad.jpg")
     )
     db.session.add(user_info)
     db.session.commit()
-    return_info = {'success': True, 'data': user_info.serialize()}
+    return_info = {'success': True, 'data': user_info.serialize(True)}
     return json.dumps(return_info), 201
 
-@app.route('/api/user/<int:user_id>/')
+
+@app.route('/api/user/<int:user_id>/', methods=['GET'])
 def get_user(user_id):
-    user = User.query.filter_by(id = user_id).first()
+    user = User.query.filter_by(id=user_id).first()
     if user is not None:
-        return_suc = {'success': True, 'data': user.serialize()}
+        return_suc = {'success': True, 'data': user.serialize(True)}
         return json.dumps(return_suc), 200
-    return_fail = {'success': False, 'error': 'Post not found!'}
+    return_fail = {'success': False, 'error': 'User not found!'}
     return json.dumps(return_fail), 404
 
-@app.route('/api/user/<int:user_id>/', methods = ['DELETE'])
+
+@app.route('/api/user/<int:user_id>/', methods=['DELETE'])
 def delete_user(user_id):
-    user = User.query.filter_by(id = user_id).first() 
+    user = User.query.filter_by(id=user_id).first()
     if user is not None:
         db.session.delete(user)
         db.session.commit()
-        return_suc = {'success': True, 'data': user.serialize()}
+        return_suc = {'success': True, 'data': user.serialize(True)}
         return json.dumps(return_suc), 200
-    return_fail = {'success': False, 'data': 'Post not found'}
+    return_fail = {'success': False, 'data': 'User not found'}
     return json.dumps(return_fail), 404
 
-@app.route('/api/user/<int:user_id>/', methods = ['POST'])
+
+@app.route('/api/user/<int:user_id>/', methods=['PUT'])
 def edit_user(user_id):
-    user = User.query.filter_by(id = user_id).first() 
+    user = User.query.filter_by(id=user_id).first()
     if user is not None:
         user_dict = json.loads(request.data)
-        user.name = user_dict.get('name',user.name)
-        user.email = user_dict.get('email',user.email)
-        user.phone = user_dict.get('phone',user.phone)
-        user.company = user_dict.get('company',user.company)
+        user.name = user_dict.get('name', user.name)
+        user.email = user_dict.get('email', user.email)
+        user.phone = user_dict.get('phone', user.phone)
+        user.company = user_dict.get('company', user.company)
+        user.imgURL = user_dict.get('imgURL', user.imgURL)
         db.session.commit()
-        return_suc = {'success': True, 'data': user.serialize()}
+        return_suc = {'success': True, 'data': user.serialize(True)}
         return json.dumps(return_suc), 200
-    return_fail = {'success': False, 'data': 'Post not found'}
+    return_fail = {'success': False, 'data': 'User not found'}
     return json.dumps(return_fail), 404
 
-@app.route('/api/user/<int:uniq_id>/<int:other_id>/colleague/', methods = ['POST'])
-def add_colleague(uniq_id,other_id):
-    person = User.query.filter_by(id = uniq_id).first() 
-    if person is not None:
-        friend = User.query.filter_by(id = other_id).first()
-        colleague = Colleagues(
-            name = friend.name,
-            email = friend.email,
-            phone = friend.phone,
-            company = friend.company,
-            user_id = person.id,
-            friend_id = friend.id,
-        )
-        user.added.append(colleague)
-        db.session.add(colleague)
+
+@app.route('/api/user/<int:your_id>/<string:their_code>/', methods=['POST'])
+def add_colleague(your_id, their_code):
+    person = User.query.filter_by(id=your_id).first()
+    contact = User.query.filter_by(code=their_code).first()
+    if person is not None and contact is not None:
+        person.contacts.append(contact)
+        db.session.add(person)
         db.session.commit()
-        return_suc = {'success': True, 'data': comment.serialize()}
+        return_suc = {'success': True, 'data': person.serialize(True)}
         return json.dumps(return_suc), 200
-    return_fail = {'success': False, 'data': 'Post not found'}
+    return_fail = {'success': False, 'data': 'Person or code not found'}
     return json.dumps(return_fail), 404
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
--- src/db.py
+++ src/db.py
@@ -1,59 +1,63 @@
+import uuid
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy() 
+db = SQLAlchemy()
+
+# A person (of type user) can have many friends. (who are also of type user).
+user_contact_assoc_table = db.Table(
+    'user_contact',
+    db.Model.metadata,
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('contact_id', db.Integer, db.ForeignKey('user.id'))
+)
+
 
 class User(db.Model):
     __tablename__ = 'user'
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String, nullable = False)
-    email = db.Column(db.String, nullable = False)
-    phone = db.Column(db.String, nullable = False)
-    company = db.Column(db.String, nullable = False)
-    added = db.relationship('Colleagues', cascade = 'delete')
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False)
+    phone = db.Column(db.String, nullable=False)
+    company = db.Column(db.String, nullable=False)
+    code = db.Column(db.String, nullable=False)
+    imgURL = db.Column(db.String, nullable=False)
+    contacts = db.relationship(
+        'User',
+        secondary=user_contact_assoc_table,
+        primaryjoin=id == user_contact_assoc_table.c.user_id,
+        secondaryjoin=id == user_contact_assoc_table.c.contact_id,
+        backref=db.backref('following')
+    )
 
     def __init__(self, **kwargs):
-        self.name = kwargs.get('name', 'Anonymous')
-        self.email = kwargs.get('email', '')
-        self.phone = kwargs.get('phone', '')
-        self.company = kwargs.get('company', '')
+        self.name = kwargs.get('name', 'Rodrigo Taipe')
+        self.email = kwargs.get('email', 'rt363@cornell.edu')
+        self.phone = kwargs.get('phone', '201-598-3168')
+        self.company = kwargs.get('company', 'Cornell University')
+        self.code = uuid.uuid4().hex[:6].upper()
+        self.imgURL = kwargs.get(
+            'imgURL', 'https://specials-images.forbesimg.com/imageserve/559d31a2e4b05c2c3431bde9/300x300.jpg?fit=scale&background=000000')
 
-
-    def serialize(self):
-        return {
-        'id': self.id,
-        'name': self.name,
-        'email': self.email,
-        'phone': self.phone,
-        'company': self.company,
-        'added': self.added
-        }
-
-class Colleagues(db.Model):
-    __tablename__ = 'colleagues'
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String, nullable = False)
-    email = db.Column(db.String, nullable = False)
-    phone = db.Column(db.String, nullable = False)
-    company = db.Column(db.String, nullable = False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
-    friend_id = db.Column(db.Integer, nullable = False)
-
-
-    def __init__(self, **kwargs):
-        self.name = kwargs.get('name', 'Anonymous')
-        self.email = kwargs.get('email', '')
-        self.phone = kwargs.get('phone', '')
-        self.company = kwargs.get('company', '')
-
-    def serialize(self):
-        return {
-        'id': self.id,
-        'friend_id': self.friend_id,
-        'name': self.name,
-        'email': self.email,
-        'phone': self.phone,
-        'company': self.company,
-        }
-
-
-
+    def serialize(self, toplevel):
+        if toplevel:
+            return {
+                'id': self.id,
+                'name': self.name,
+                'phone': self.phone,
+                'email': self.email,
+                'company': self.company,
+                'code': self.code,
+                'imgURL': self.imgURL,
+                'contacts': [u.serialize(False) for u in self.contacts]
+            }
+        else:
+            return {
+                'id': self.id,
+                'name': self.name,
+                'phone': self.phone,
+                'email': self.email,
+                'company': self.company,
+                'code': self.code,
+                'imgURL': self.imgURL,
+                'contacts': []
+            }
