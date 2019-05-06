c11a58d875572fb8d4cef9d112c7bd44d2f29c89
Rodrigo Taipe <rt363@cornell.edu>
2019-05-05 22:39:13 -0400

    Add image capabilites

--- src/app.py
+++ src/app.py
@@ -1,15 +1,15 @@
 import json
 import os
+import cv2
 from db import db, User
 from flask import Flask, request, send_from_directory
-
 
 
 app = Flask(__name__)
 db_filename = 'business.db'
 UPLOAD_FOLDER = 'uploads'
 
-PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'))
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__)))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///%s' % db_filename
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
@@ -24,6 +24,7 @@ def get_all_users():
     users = User.query.all()
     res = {'success': True, 'data': [user.serialize(True) for user in users]}
     return json.dumps(res), 200
+
 
 @app.route('/api/users/', methods=['POST'])
 def create_user():
@@ -44,6 +45,7 @@ def create_user():
     return_info = {'success': True, 'data': user_info.serialize(True)}
     return json.dumps(return_info), 201
 
+
 @app.route('/api/user/<int:user_id>/', methods=['GET'])
 def get_user(user_id):
     user = User.query.filter_by(id=user_id).first()
@@ -52,6 +54,7 @@ def get_user(user_id):
         return json.dumps(return_suc), 200
     return_fail = {'success': False, 'error': 'User not found!'}
     return json.dumps(return_fail), 404
+
 
 @app.route('/api/user/<int:user_id>/', methods=['DELETE'])
 def delete_user(user_id):
@@ -63,6 +66,7 @@ def delete_user(user_id):
         return json.dumps(return_suc), 200
     return_fail = {'success': False, 'data': 'User not found'}
     return json.dumps(return_fail), 404
+
 
 @app.route('/api/user/<int:user_id>/', methods=['POST'])
 def edit_user(user_id):
@@ -81,6 +85,7 @@ def edit_user(user_id):
     return_fail = {'success': False, 'data': 'User not found'}
     return json.dumps(return_fail), 404
 
+
 @app.route('/api/user/<int:your_id>/<string:their_code>/', methods=['POST'])
 def add_contact(your_id, their_code):
     person = User.query.filter_by(id=your_id).first()
@@ -97,6 +102,7 @@ def add_contact(your_id, their_code):
         return json.dumps(return_suc), 200
     return_fail = {'success': False, 'data': 'User or Contact not found'}
     return json.dumps(return_fail), 404
+
 
 @app.route('/api/user/<int:your_id>/<int:their_id>/', methods=['DELETE'])
 def del_contact(your_id, their_id):
@@ -116,13 +122,13 @@ def del_contact(your_id, their_id):
     return json.dumps(return_fail), 404
 
 
-@app.route('/images/', methods = ['POST'])
-def upload_image():
+@app.route('/api/images/<int:your_id>', methods=['POST'])
+def upload_image(your_id):
     if UPLOAD_FOLDER is not None:
         file = request.files[UPLOAD_FOLDER]
         file.save(os.path.join(PROJECT_ROOT, UPLOAD_FOLDER, file.filename))
-        newImgURL = "http://34.73.24.69/images/" + file,filename
-        user=db.User.filter_by(id=int(file.filename)).first()
+        newImgURL = "http://34.73.24.69/images/" + str(your_id)
+        user = User.query.filter_by(id=your_id).first()
         user.imgURL = newImgURL
         db.session.add(user)
         db.session.commit()
@@ -131,7 +137,8 @@ def upload_image():
     res = {'success': False, 'data': 'No File Found'}
     return json.dumps(res), 404
 
-@app.route('/images/<int:id>', methods = ['GET'])
+
+@app.route('/api/images/<int:id>', methods=['GET'])
 def get_image(id):
     path = os.path.join(PROJECT_ROOT, UPLOAD_FOLDER)
     filepath = os.path.join(path, str(id)+'.png')
@@ -139,11 +146,12 @@ def get_image(id):
     if path is not None:
         if os.path.isfile(filepath):
             name = str(id) + '.png'
-            return send_from_directory(directory=path, filename = name)
+            return send_from_directory(directory=path, filename=name)
         else:
             return json.dumps({'error': 'Not a valid id'}), 404
     else:
         return json.dumps({'error': 'Not a valid path'}), 404
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
